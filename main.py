652489d92b432019454e656e20b3b9177f4897e2
iyotti <neuroooo100@gmail.com>
2019-02-15 23:29:39 +0900

    csvファイル名に日付を追加

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import requests, sys, webbrowser, bs4, os
 import csv
 import requests
+import datetime
 
 def scraping_hatena():
 	url = 'http://b.hatena.ne.jp/'
@@ -12,21 +13,26 @@ def scraping_hatena():
 	soup = bs4.BeautifulSoup(res.content,'html.parser')
 
 	title = soup.select('div > h3 > a.js-keyboard-openable')
-	if title == []: print("見つからなかった")
-
+	if title == []:
+		print("見つからなかった")
+		return
+	arr = []
 	for i in range(len(title)):
-		arr = [title[i].getText(),title[i].get('href')]
-		output(arr)
+		tmp = [title[i].getText(),title[i].get('href')]
+		arr.append(tmp)
+	output(arr)
 
 
 def output(arr):
-	os.makedirs("csv",exist_ok=True)
+	csv_name = 'hatena_log_'+datetime.datetime.now().strftime('%Y%m%d')+'.csv'
 
-	with open(os.path.join('csv','hatena_log.csv'),'a',encoding="utf-8") as csv_file:
+	with open(os.path.join('csv',csv_name),'a',encoding="utf-8") as csv_file:
 		csv_writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
-		csv_writer.writerow(arr)
+		csv_writer.writerows(arr)
 		csv_file.close()
 
 
 if __name__ == '__main__':
+	os.makedirs("csv",exist_ok=True)
+
 	scraping_hatena()
